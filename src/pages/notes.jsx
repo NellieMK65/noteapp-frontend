b48b383b58a519b1d58525e137de91724af17e29
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-07-17 12:42:18 +0300

    initiate payment

--- src/pages/notes.jsx
+++ src/pages/notes.jsx
@@ -1,19 +1,67 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { BASE_URL } from "@/utils";
+import toast from "react-hot-toast";
 
 export default function NotesPage() {
-	const handleAddNote = () => {
+	let toastId = null;
+	let intervalId = null;
+
+	const checkPayment = (id) => {
+		fetch(`${BASE_URL}/payments/check/${id}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				// this stops shot polling
+
+				clearInterval(intervalId);
+
+				if (data.data.ResultCode == "0") {
+					toast.success("Payment successfull", { id: toastId });
+				} else {
+					toast.error("Payment not successfull", { id: toastId });
+				}
+			});
+	};
+
+	const handleInitiatePayment = () => {
 		const accessToken = localStorage.getItem("session");
 
-		fetch(`${BASE_URL}/entries`, {
+		toastId = toast.loading("Initiating stk push");
+
+		fetch(`${BASE_URL}/payments`, {
 			method: "POST",
-			body: JSON.stringify({}),
+			body: JSON.stringify({
+				phone: "",
+			}),
 			headers: {
 				Authorization: `Bearer ${accessToken}`,
 				"Content-Type": "application/json",
 				Accept: "application/json",
 			},
-		});
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				toast.loading("Confirming payment", { id: toastId });
+				// initiate shot polling
+				// The interval should only run for at most 2 mins
+				intervalId = setInterval(
+					() => checkPayment(data.data.CheckoutRequestID),
+					10_000
+				);
+			});
 	};
 
-	return <>Notes</>;
+	return (
+		<div>
+			<Input />
+
+			<Button onClick={() => handleInitiatePayment()}>Pay</Button>
+		</div>
+	);
 }
